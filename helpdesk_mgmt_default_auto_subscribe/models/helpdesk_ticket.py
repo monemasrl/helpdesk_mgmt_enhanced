48351c863fd0562f08a616c9ddf56a34ea4318a1
Andrea Bettarini <bettarini@monema.it>
2022-08-30 12:29:20 +0200

    On ticket creation subscribe the partner (incomplete implementation)

---
+++ helpdesk_mgmt_default_auto_subscribe/models/helpdesk_ticket.py
@@ -0,0 +1,29 @@
+###############################################################################
+#    License, author and contributors information in:                         #
+#    __manifest__.py file at the root folder of this module.                  #
+###############################################################################
+
+from odoo import models
+
+
+class HelpdeskTicket(models.Model):
+    _inherit = "helpdesk.ticket"
+
+    def create(self, vals):
+        partner_email = vals.get("partner_email", False)
+
+        if partner_email:
+            partner_ids = [
+                p.id
+                for p in self.env["mail.thread"]._mail_find_partner_from_emails(
+                    [partner_email], records=self, force_create=True
+                )
+                if p
+            ]
+
+            ticket = super().create(vals)
+            ticket.message_subscribe(partner_ids)
+
+            return ticket
+
+        return super().create(vals)
