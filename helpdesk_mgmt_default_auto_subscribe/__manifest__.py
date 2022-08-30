0b9366c6a1475b621beb2c0c83384bc79d448ca2
Andrea Bettarini <bettarini@monema.it>
2022-08-30 12:05:38 +0200

    Setup pre-commit hook

---
+++ helpdesk_mgmt_default_auto_subscribe/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Helpdesk Auto Subscribe",
+    "summary": "When the ticket is created the sender is added as follower to the ticket",
+    "version": "14.0.1.0.0",
+    "author": "Monema S.r.l.",
+    "maintainer": "Monema S.r.l.",
+    "contributors": ["Andrea Bettarini <bettarini@monema.it>"],
+    "website": "https://github.com/OCA/helpdesk",
+    "license": "Other OSI approved licence",
+    "category": "After-Sales",
+    "application": False,
+    "depends": ["helpdesk_mgmt"],
+    "data": [],
+}
