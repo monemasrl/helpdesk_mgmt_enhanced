cff1c45eaaf3c5ce70089914d99dcf4be19a5a3a
Andrea Bettarini <bettarini@monema.it>
2022-09-05 13:04:37 +0200

    Fix check_ticket_sla in ticket

--- helpdesk_mgmt_enhanced_sla/models/helpdesk_enhanced_ticket.py
+++ helpdesk_mgmt_enhanced_sla/models/helpdesk_enhanced_ticket.py
@@ -4,6 +4,7 @@
 ###############################################################################
 
 import logging
+from datetime import datetime
 
 from odoo import api, fields, models
 
@@ -34,4 +35,42 @@ class HelpdeskTicket(models.Model):
     def _onchange_sla_id(self):
         self.team_id = self.sla_id.team_id
         _logger.debug("SLA changed")
-        self.sla_id.check_ticket_sla2()
+        self.check_ticket_sla()
+
+    def check_ticket_sla(self):
+        # for ticket in ticket_ids.filtered(lambda t: not t.stage_id.closed):
+        for ticket in self:
+            deadline = ticket.create_date
+            working_calendar = ticket.team_id.resource_calendar_id
+
+            if self.days > 0:
+                deadline = working_calendar.plan_days(
+                    self.days + 1, deadline, compute_leaves=True
+                )
+                create_date = ticket.create_date
+
+                deadline = deadline.replace(
+                    hour=create_date.hour,
+                    minute=create_date.minute,
+                    second=create_date.second,
+                    microsecond=create_date.microsecond,
+                )
+
+                deadline_for_working_cal = working_calendar.plan_hours(0, deadline)
+
+                if (
+                    deadline_for_working_cal
+                    and deadline.day < deadline_for_working_cal.day
+                ):
+                    deadline = deadline.replace(
+                        hour=0, minute=0, second=0, microsecond=0
+                    )
+
+            deadline = working_calendar.plan_hours(
+                self.hours, deadline, compute_leaves=True
+            )
+            ticket.sla_deadline = deadline
+            if ticket.sla_deadline < datetime.today().now():
+                ticket.sla_expired = True
+            else:
+                ticket.sla_expired = False
